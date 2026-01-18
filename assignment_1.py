55dcc1e2bcb30ad014a183038c7190aed137dc14
Shiva Mani <mop91838@gmail.com>
2026-01-18 16:40:48 +0000

    update

---
+++ assignment_1.py
@@ -0,0 +1,81 @@
+# Q1 
+# name=input("enter your good name :");
+# age = input("enter your age:");
+# print("hello", name, ", you age is",age);
+
+
+
+# Q2
+# a= int(input("enter a number:"))
+# b= int(input("enter a number:"))
+# print(a+b)
+# print(a*b)
+# print(a/b)
+# print(a-b)
+# print(a%b)
+
+
+
+#Q3
+# a= int(input("enter:"))
+# b= int(input("enter:"))
+# c= float(input("enter a float:"))
+# print("avg is :", (a+b+c)/2)
+
+
+
+# 4
+# s = input("Enter a number: ")
+# i = int(s)
+# f = float(s)
+# st = str(s)
+# print(i)
+# print(f)
+# print(st)
+
+
+
+# 5
+# x =10+3*2**2
+# print(x)
+# print("operator priority first * then power and last +")
+
+
+# 6
+# a= input("enter a number:")
+# b = input("enter another number:")
+# temp = a;
+# a= b;
+# b= temp;
+# print(a)
+# print(b)
+
+
+# 7
+# c = input("enter temperature in c:")
+# c = float(c)
+# f=(c*(9/5))+32
+# print(f)
+
+
+
+# 8
+# r =int(input("enter a number:"))
+# a= 3.14 * (r**(2))
+# print(a)
+
+
+# 9
+# p=int(input("enter a p:"))
+# r=int(input("enter a r:"))
+# t=int(input("enter a t:"))
+# S=(p*r*t)/100
+# print(r)
+
+
+
+# 10
+# p=(input("enter a float:"))
+# i, d = p.split(".")
+# print(i)
+# print(".", d)
