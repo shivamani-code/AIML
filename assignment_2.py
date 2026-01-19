0678c2b8a266bbfdad333e92d08430b12ec1adad
Shiva Mani <mop91838@gmail.com>
2026-01-19 16:03:21 +0000

    assignment 2 py 2

---
+++ assignment_2.py
@@ -0,0 +1,104 @@
+#1
+# salary = (input("enter your income:"))
+# print("finaltax rate")
+# if salary < "30,000":
+#     print("5%")
+# elif salary > "30,000" and salary < "70,000":
+#     print("15%")
+# else :
+#     print("25%")
+
+
+
+#2
+# a=int(input("enter a number"))
+# b=int(input("enter a number"))
+# i=0
+# for i in range(a,b,i+1):
+#    if i%2==0: 
+#         print(i)
+
+
+#3
+# def print_digits(n):
+#     while n > 0:
+#         digit = n % 10
+#         print(digit)
+#         n = n // 10
+
+# n = int(input("Enter a number: "))
+# print_digits(n)
+
+
+
+#4
+# def count_digits(n):
+#     count = 0
+#     while n > 0:
+#         count += 1
+#         n = n // 10
+#     return count
+
+# n = int(input("Enter a number: "))
+# print(count_digits(n))
+
+
+
+#5
+# def sum():
+#     n = int(input("enter a number:"))
+#     m = int(input("enter a number:"))
+#     t = n+m
+#     print("sum is:",t)
+
+# sum()
+
+
+
+#6
+# n = int(input("enter a start:"))
+# e = int(input("enter a end:"))
+# for i in range(n,e,1):
+#     if i%3==0 and i%5==0:
+#         print(i)
+
+
+#7
+# print("welcome to infinte loop (Q() to quite) ")
+# while True:
+#    p =(input("enter a mumber:"))
+#    if p == "q":
+#         break
+#    n=int(p)
+#    if p > "0":
+#       print("+ve")
+#    else:
+#       print("-ve")
+      
+
+
+#8
+# def calculator(a, b, operation):
+#     match operation:
+#         case "+":
+#             print(a+b)
+#         case "-":
+#             print(a-b)
+#         case "/":
+#             print(a/b)
+#         case "%":
+#             print(a%b)
+#         case "*":
+#             print(a*b)
+#         case "**":
+#             print(a**b)
+
+# a=int(input("enter a number:"))
+# b=int(input("enter a number:"))
+# operation = input("enter a number:")
+# calculator(a,b,operation)
+        
+
+
+#9
+
