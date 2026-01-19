fceba8c86104cace559a281fa8a7c80b4cbfd473
Shiva Mani <mop91838@gmail.com>
2026-01-19 12:53:46 +0000

    practice py 2

---
+++ py_part2.py
@@ -0,0 +1,38 @@
+# age =  int(input("enter you age:"))
+# if age < 13:
+#     print("child")
+# elif age <18:
+#     print("adult")
+# else :
+#     print("young")
+
+
+
+
+# name = input("enter name of user:")
+# pas = (input("enter name of pass:"))
+# if name == 'mani' and pas == 'admin' :
+#     print("pass")
+# else :
+#     print("incorrect")
+
+
+
+# n = int(input("enter a number :"))
+# if n%5==0:
+#     print("it is multiple")
+# else :
+#     print("not")
+
+
+# colour = input ("enter a colour: ")
+# match colour:
+#     case "green":
+#         print("okay")
+#     case "bule":
+#         print("not ok")
+#     case _: 
+#         print("not")
+
+
+
