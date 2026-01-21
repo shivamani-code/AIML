321682e12bcfc86635bbc284b02c98b229dc9b94
Shiva Mani <mop91838@gmail.com>
2026-01-21 06:19:11 +0000

    update py3

---
+++ py-3.py
@@ -0,0 +1,15 @@
+# word = "mani"
+# print(word[3])
+# print(len(word))
+
+
+# word ="mani"
+# print(word[2:4])
+
+
+# a=int("4")
+# b=int("3")
+# c=a+b
+# print("sum of {} & {} is {}".format(a,b,c))
+
+
