792e2a2c63fbe1f3a9b5036c8a999c0288bc32d4
Shiva Mani <mop91838@gmail.com>
2026-01-23 16:10:13 +0000

    update py

--- py-3.py
+++ py-3.py
@@ -13,3 +13,35 @@
 # print("sum of {} & {} is {}".format(a,b,c))
 
 
+# name = "MANI"
+# print(name[1:2:1])
+
+
+# name=["mani", "age", "price"]
+# print(name)
+
+# num =[1,2,3,4,5]
+# print(len(num))
+# num.append(18)
+# print(num)
+# num.insert(4,3)
+# print(num)
+# num.sort()
+# print(sum)
+# num.reverse()
+# print(num)
+
+
+
+# numbers = [1, 2, 3, 4, 5]
+
+# def find_num(x):
+#     for i in numbers:
+#         if i == x:
+#             return i
+
+# print(find_num(5))
+
+
+
+
